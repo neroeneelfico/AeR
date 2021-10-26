08bb0917887ec73b5494125b2bdf91bd6ee6467e
Marcello Vecchio <marcello.vecchio92@gmail.com>
2021-10-27 00:19:14 +0200

    Inizio

---
+++ app/src/main/java/com/example/aer_anestesiaerianimazione/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.aer_anestesiaerianimazione
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val navView = findViewById<NavigationView>(R.id.navView)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.pH -> Toast.makeText(applicationContext, "Cliccato pH",Toast.LENGTH_SHORT).show()
+                R.id.bicarbonati -> Toast.makeText(applicationContext, "Cliccato bicarbonati",Toast.LENGTH_SHORT).show()
+
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
